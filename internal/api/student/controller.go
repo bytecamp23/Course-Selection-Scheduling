ac21cfbaaabaf39d5cf03a91e62ed555f417de8a
lmessi <yzg0808@outlook.com>
2022-02-14 21:46:05 +0800

    move Consume to rabbitmq.go

--- internal/api/student/controller.go
+++ internal/api/student/controller.go
@@ -2,23 +2,65 @@ package student
 
 import (
 	"Course-Selection-Scheduling/internal/global"
-	"Course-Selection-Scheduling/pkg/mydb"
+	"Course-Selection-Scheduling/pkg/myredis"
+	"Course-Selection-Scheduling/pkg/rabbitmq"
 	"encoding/json"
+	"fmt"
+	"github.com/gin-gonic/gin"
 	"log"
+	"net/http"
 )
 
-func Consume(msgByte []byte) {
-	//解析message
-	var msg global.BookCourseRequest
-	err := json.Unmarshal(msgByte, &msg)
+func BookCourse(c *gin.Context) {
+	requestData := global.BookCourseRequest{}
+	if err := c.BindJSON(&requestData); err != nil { //若绑定出错
+		c.JSON(
+			http.StatusOK,
+			global.BookCourseResponse{Code: global.ParamInvalid},
+		)
+		return
+	}
+	//生成userCourse ,防止同一个用户多次抢同一门课
+	stuCourse := fmt.Sprintf("%s_%s", requestData.StudentID, requestData.CourseID)
+	_, err := myredis.GetFromRedis(stuCourse)
+	if err != nil {
+		myredis.PutToRedis(stuCourse, "true", 5) //5秒内只能抢一次
+	} else {
+		c.JSON(
+			http.StatusOK,
+			global.BookCourseResponse{Code: global.RepeatRequest},
+		)
+		return
+	}
+
+	//查询课程余量并减库存 , 数据库操作送入消息队列中
+	value, err := myredis.DecrForRedis(requestData.CourseID)
+	if err != nil {
+		c.JSON(
+			http.StatusOK,
+			global.BookCourseResponse{Code: global.UnknownError},
+		)
+		return
+	}
+	if value.(int) < 0 {
+		c.JSON(
+			http.StatusOK,
+			global.BookCourseResponse{Code: global.CourseNotAvailable},
+		)
+		return
+	}
+
+	//放到消息队列中,进行数据库操作
+	msgByte, err := json.Marshal(requestData)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	//扣减课程余量
-	global.MysqlClient.AutoMigrate(&mydb.Course{}) //迁移表到Course
-	var course mydb.Course
-	global.MysqlClient.Model(&course).Update("CourseId", course.Cap-1)
-	//插入课表
-	global.MysqlClient.AutoMigrate(&mydb.SelectCourse{}) //迁移表到SelectCourse
-	global.MysqlClient.Create(&mydb.SelectCourse{StudentId: msg.StudentID, CourseId: msg.CourseID})
+	rmq := rabbitmq.NewRabbitMQSimple("bookcourse")
+	rmq.PublishSimple(msgByte)
+
+	//回传OK
+	c.JSON(
+		http.StatusOK,
+		global.BookCourseResponse{Code: global.OK},
+	)
 }
--- pkg/rabbitmq/rabbitmq.go
+++ pkg/rabbitmq/rabbitmq.go
@@ -1,7 +1,9 @@
 package rabbitmq
 
 import (
-	"Course-Selection-Scheduling/internal/api/student"
+	"Course-Selection-Scheduling/internal/global"
+	"Course-Selection-Scheduling/pkg/mydb"
+	"encoding/json"
 	"fmt"
 	"github.com/streadway/amqp"
 	"log"
@@ -110,9 +112,24 @@ func (r *RabbitMQ) ConsumeSimple() {
 	go func() {
 		for d := range msgs {
 			log.Printf("Received a message: %s", d.Body)
-			student.Consume(d.Body)
+			Consume(d.Body)
 		}
 	}()
 	log.Printf("[*] Waiting for messages, To exit press CTRL+C")
 	<-forever
+}
+func Consume(msgByte []byte) {
+	//解析message
+	var msg global.BookCourseRequest
+	err := json.Unmarshal(msgByte, &msg)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//扣减课程余量
+	global.MysqlClient.AutoMigrate(&mydb.Course{}) //迁移表到Course
+	var course mydb.Course
+	global.MysqlClient.Model(&course).Update("CourseId", course.Cap-1)
+	//插入课表
+	global.MysqlClient.AutoMigrate(&mydb.SelectCourse{}) //迁移表到SelectCourse
+	global.MysqlClient.Create(&mydb.SelectCourse{StudentId: msg.StudentID, CourseId: msg.CourseID})
 }
